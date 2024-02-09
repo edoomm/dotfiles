f9ab5005397dd911c35731f86477dcb114e18a67
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-02-08 19:36:37 -0600

    Added path and temporary fix for keyboard config

---
+++ scripts/ctrl2caps.sh
@@ -0,0 +1,14 @@
+# For some reason executing this once does not take effect
+# This is just a temp solution
+
+setxkbmap latam
+setxkbmap -option ctrl:nocaps
+killall xcape ; xcape -e 'Control_L=Escape'
+
+setxkbmap latam
+setxkbmap -option ctrl:nocaps
+killall xcape ; xcape -e 'Control_L=Escape'
+
+setxkbmap latam
+setxkbmap -option ctrl:nocaps
+killall xcape ; xcape -e 'Control_L=Escape'
