2bdb0e5e73587a3bdce7f23d3045f5b14c288a16
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-02-17 22:41:27 -0600

    Added keymaps

--- config/nvim/lua/core/remap.lua
+++ config/nvim/lua/core/remap.lua
@@ -1,3 +1,15 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader>br", function()
+	vim.cmd("%bd|e#|bd#")
+end)
