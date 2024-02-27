0d6eb0a8956cdf1adf0b168c8fd663e6e4f61d93
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-02-27 15:35:54 -0600

    Changed keymap

--- config/nvim/plugin/autolist.lua
+++ config/nvim/plugin/autolist.lua
@@ -4,7 +4,7 @@ vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
 vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
 vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
 vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+vim.keymap.set("n", "<leader>ar", "<cmd>AutolistRecalculate<cr>")
 
 -- functions to recalculate list on edit
 vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
