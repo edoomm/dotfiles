d162caf40b86fe0185e6dbc210af7cd0e2c6e102
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-04-03 20:43:47 -0600

    Added options

--- link.sh
+++ link.sh
@@ -1,20 +1,57 @@
 #!/bin/bash
 
+option="$1"
 df_path="$(pwd)"
 
 mkdir -p "$HOME/.config/tmux"
 mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/.config/dunst"
 
-ln -fs "$df_path/bash/.bash_profile" "${HOME}/.bash_profile" \
-	&& ln -fs "$df_path/bash/.bashrc" "${HOME}/.bashrc" \
-	&& ln -fs "$df_path/git/.gitconfig" "${HOME}/.gitconfig" \
-	&& ln -fs "$df_path/xserv/.xinitrc" "${HOME}/.xinitrc" \
-	&& ln -fs "$df_path/config/alacritty/alacritty.toml" "${HOME}/.config/alacritty/alacritty.toml" \
-	&& ln -fs "$df_path/config/i3/config" "${HOME}/.config/i3/config" \
-	&& ln -fs "$df_path/config/i3blocks/config" "${HOME}/.config/i3blocks/config" \
-	&& ln -fs "$df_path/config/neofetch/config.conf" "${HOME}/.config/neofetch/config.conf" \
-	&& ln -fs "$df_path/config/nvim" "${HOME}/.config" \
-	&& ln -fs "$df_path/config/tmux/tmux.conf" "${HOME}/.config/tmux/tmux.conf" \
-	&& ln -fs "$df_path/bin/openprj" "${HOME}/.local/bin/openprj" \
-	&& ln -fs "$df_path/config/dunstrc" "${HOME}/.config/dunst/dunstrc" \
+link_file() {
+	source_path="$df_path/$1"
+	target_path="$HOME/$2"
+	if [ -f "$source_path" ] || [ -d "$source_path" ] ; then
+		ln -fs "$source_path" "$target_path"
+		echo "Linked: $source_path to $target_path"
+	else
+		echo "Error: Source file or directory '$source_path' not found."
+	fi
+}
+
+declare -A links_source=(
+	["bashrc"]="bash/.bashrc"
+	["bashpf"]="bash/.bash_profile"
+	["git"]="git/.gitconfig"
+	["xinitrc"]="xserv/.xinitrc"
+	["alacritty"]="config/alacritty/alacritty.toml"
+	["i3"]="config/i3/config"
+	["i3blocks"]="config/i3blocks/config"
+	["neofetch"]="config/neofetch/config.conf"
+	["nvim"]="config/nvim"
+	["tmux"]="config/tmux/tmux.conf"
+	["openprj"]="bin/openprj"
+	["dunst"]="config/dunstrc"
+)
+
+declare -A links_target=(
+	["bashrc"]=".bashrc"
+	["bashpf"]=".bash_profile"
+	["git"]=".gitconfig"
+	["xinitrc"]=".xinitrc"
+	["alacritty"]=".config/alacritty/alacritty.toml"
+	["i3"]=".config/i3/config"
+	["i3blocks"]=".config/i3blocks/config"
+	["neofetch"]=".config/neofetch/config.conf"
+	["nvim"]=".config"
+	["tmux"]=".config/tmux/tmux.conf"
+	["openprj"]=".local/bin/openprj"
+	["dunst"]=".config/dunst/dunstrc"
+)
+
+# Check if the option is valid
+if [[ ! ${links_source[$option]} ]]; then
+	echo "Error: Invalid option '$option'. Available options: ${!links_source[@]}"
+	exit 1
+fi
+
+link_file "${links_source[$option]}" "${links_target[$option]}"
