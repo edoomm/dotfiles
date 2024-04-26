58b29013ad30694af2cc62b0a2e96f64c1d6f790
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-04-26 10:57:20 -0600

    Changed dunstify to notify-send Since action is not handled

--- scripts/displayupdates.sh
+++ scripts/displayupdates.sh
@@ -1,15 +1,6 @@
-update_packages () { dunstify "TODO: Updating..."; }
-
 updates=$(checkupdates | wc -l)
+message="$updates system updates available"
 
 if (( "$updates" != "0" )); then
-	ACTION=$(
-	dunstify -h string:x-dunst-stack-tag:updates \
-		-A 'updated, default' \
-		"$updates system updates available"
-	)
-
-	if (( "$ACTION" == "2" )); then
-		update_packages
-	fi
+	notify-send -u normal -c system-updates "$message"
 fi
