1f40c55930f4becc607f2771dac6e81a5a710c34
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-05-09 18:32:38 -0600

    Removed temporal solution I believe with fresh arch install, this was solved

--- scripts/ctrl2caps.sh
+++ scripts/ctrl2caps.sh
@@ -1,14 +1,3 @@
-# For some reason executing this once does not take effect
-# This is just a temp solution
-
-setxkbmap latam
-setxkbmap -option ctrl:nocaps
-killall xcape ; xcape -e 'Control_L=Escape'
-
-setxkbmap latam
-setxkbmap -option ctrl:nocaps
-killall xcape ; xcape -e 'Control_L=Escape'
-
 setxkbmap latam
 setxkbmap -option ctrl:nocaps
 killall xcape ; xcape -e 'Control_L=Escape'
