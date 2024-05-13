65220e523f8da738f07d0e6f40e0365da1915f05
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-05-12 18:27:48 -0600

    Added error handling

--- scripts/displayupdates.sh
+++ scripts/displayupdates.sh
@@ -1,6 +1,14 @@
-updates=$(checkupdates | wc -l)
-message="$updates system updates available"
+num_updates=$(checkupdates | wc -l)
+message="$num_updates system updates available"
 
-if (( "$updates" != "0" )); then
+if (( "$num_updates" != "0" )); then
 	notify-send -u normal -c system-updates "$message"
+else 
+	checkupdates
+	if (( "$?" == "1" )); then
+		error_msg_1="Cannot fetch system updates (ERR_NO: 1)"
+		error_hnt_1="Hint: Possibly no internet connection"
+		msg="$(echo $error_msg_1 && echo $error_hnt_1)"
+		notify-send -u normal -c system-updates "$msg"
+	fi
 fi
