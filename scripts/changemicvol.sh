5c1856c87d33e278bcbb80e058283cd890874362
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-05-12 19:05:01 -0600

    Modified microphone mute toggle through script

--- scripts/changemicvol.sh
+++ scripts/changemicvol.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-volume=$(amixer set Capture $1%$2 | tail -n 1 | awk '{ print $5, $6 }')
+if (( "$1" == "mute" )); then
+	pactl set-source-mute @DEFAULT_SOURCE@ toggle
+	volume=$(amixer get Capture | tail -n 1 | awk '{ print $5, $6 }')
+else
+	volume=$(amixer set Capture $1%$2 | tail -n 1 | awk '{ print $5, $6 }')
+fi
 message="Microphone volume changed to $volume"
 
 notify-send -u normal -c volume-mic-changed -t 2000 "$message"
