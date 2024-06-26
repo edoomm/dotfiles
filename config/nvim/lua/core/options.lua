9791f3698afe8f0f1ecd080e91444bc37042a1b1
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-06-25 20:22:02 -0600

    Added other options From ThePrimeagen https://www.youtube.com/watch?v=w7i4amO_zaE&t=1297s The ones I found interesting

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -9,3 +9,8 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
