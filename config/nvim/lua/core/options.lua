7398915b3818e2decd653b0e6357d97daea2a016
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-06-25 20:18:05 -0600

    Added tab identations config From ThePrimeagen https://www.youtube.com/watch?v=w7i4amO_zaE&t=1297s

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -3,3 +3,9 @@ vim.opt.relativenumber = true
 vim.opt.hidden = false
 
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
