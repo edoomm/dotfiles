acd95eba3017d05d2f1be896fdccdeddb1d17a2d
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-10-10 17:05:16 -0600

    Changed primary monitor retrieval

--- scripts/toggle-monitor.sh
+++ scripts/toggle-monitor.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 output="HDMI-1-1"
+primary=$(xrandr | grep primary | awk '{print $1}')
 res="1360"
 monitor_connected=$(xrandr | grep "$output connected $res")
 
 if [[ -z "$monitor_connected" ]]; then
-	xrandr --output $output --auto --output eDP-1 --auto --left-of $output
+	xrandr --output $output --auto --output $primary --auto --left-of $output
 else
 	xrandr --output $output --off
 fi
